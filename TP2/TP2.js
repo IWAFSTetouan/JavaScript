91b978e7f2d835dabbb9302a15a6dcc2bc41e89c
enaanai <aenaanai@uae.ac.ma>
2024-11-14 21:10:47 +0100

    Persist

--- TP2/TP2.js
+++ TP2/TP2.js
@@ -11,24 +11,52 @@ function ajouterEtudiant(){
     etudiant=Object.fromEntries(formData.entries())
     console.log(etudiant)
 
-    //Ajouter l'étudiant dans le tableau
-    liste_etudiants=document.getElementById('liste_etudiants')
-    ligne=document.createElement("tr")
-    idCell=document.createElement("td"); idCell.innerHTML=etudiant.id
-    nomCell=document.createElement("td"); nomCell.innerHTML=etudiant.nom
-    ageCell=document.createElement("td"); ageCell.innerHTML=etudiant.age
-    actionCell=document.createElement("td")
-    deleteImg=document.createElement("img")
-    deleteImg.setAttribute("src","https://files.softicons.com/download/toolbar-icons/vista-base-software-icons-2-by-icons-land/png/256x256/DeleteRed.png")
-    deleteImg.setAttribute("onclick","deleteEtudiant(event)")
-    actionCell.appendChild(deleteImg)
-    ligne.appendChild(idCell);ligne.appendChild(nomCell);
-    ligne.appendChild(ageCell);ligne.appendChild(actionCell)
-    liste_etudiants.appendChild(ligne)
+    addToTable(etudiant)
+    //Vider les zones de texte
     document.getElementById('form_etudiant').reset()
+    //Enregistrer l'étudiant dans localStorage
+    persist(etudiant)
+
 }
 
 function deleteEtudiant(e){ 
     ligne=e.target.parentElement.parentElement
     ligne.remove()
+}
+
+function persist(etudiant){
+    localStorage.setItem(etudiant.id,JSON.stringify(etudiant))
+}
+
+function loadData(){
+    for(i=0;i<localStorage.length;i++){
+        key=localStorage.key(i)
+        etudiantJson= localStorage.getItem(key)
+        etudiant=JSON.parse(etudiantJson)
+        addToTable(etudiant)
+    } 
+}
+
+window.addEventListener('load',()=>{
+    loadData()
+})
+
+function addToTable(etudiant){
+    //Ajouter l'étudiant dans le tableau
+    liste_etudiants=document.getElementById('liste_etudiants')
+    ligne=document.createElement("tr")
+    //Créer les cellules de la ligne à ajouter
+    idCell=document.createElement("td"); idCell.innerHTML=etudiant.id
+    nomCell=document.createElement("td"); nomCell.innerHTML=etudiant.nom
+    ageCell=document.createElement("td"); ageCell.innerHTML=etudiant.age
+    actionCell=document.createElement("td")
+    //Créer l'image de suppression d'une ligne
+    deleteImg=document.createElement("img")
+    deleteImg.setAttribute("src","https://files.softicons.com/download/toolbar-icons/vista-base-software-icons-2-by-icons-land/png/256x256/DeleteRed.png")
+    deleteImg.setAttribute("onclick","deleteEtudiant(event)")
+    //Ajouter chaque élément HTML à son élément parent
+    actionCell.appendChild(deleteImg)
+    ligne.appendChild(idCell);ligne.appendChild(nomCell);
+    ligne.appendChild(ageCell);ligne.appendChild(actionCell)
+    liste_etudiants.appendChild(ligne)
 }
